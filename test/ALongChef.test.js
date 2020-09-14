b1e1bb67ca287399517e4ac12e873da0ba8c2314
psyduckasa <duckpsy@protonmail.com>
2020-09-15 00:27:19 +0800

    Generate different returns from 1-126,000 blocks, all covered by the test.

--- test/ALongChef.test.js
+++ test/ALongChef.test.js
@@ -51,7 +51,8 @@ contract('LongChef', ([alice, bob, carol, dev, minter]) => {
             assert.equal((await this.lp.balanceOf(bob)).valueOf(), '1000');
         });
 
-        it('should give out LONGs only block number in 21000', async () => {
+        it('should give out LONGs block number all', async () => {
+            // For this test, you have to change the blocksPerStage of LongChef.sol#154 to 21 and make sure the ganache starts at block 0!
             /*
             The first 126,000 blocks (~18 days) is the bonus period. We divide the bonus period into 6 stages, each of which is 21,000 blocks (~3 days). The distribution of LONG tokens in these 6 stages is planned as follows:
             1~21,000 blocks, 1000 tokens minted per block
@@ -62,26 +63,57 @@ contract('LongChef', ([alice, bob, carol, dev, minter]) => {
             105,001~126,000 blocks, 500 tokens minted per block
             After the bonus period ends, 100 tokens will be minted per block.
             */
-            this.chef = await LongChef.new(this.long.address, dev, '100', '1', '126000', { from: alice });
+            this.chef = await LongChef.new(this.long.address, dev, '100', '36', '162', { from: alice });
             await this.long.transferOwnership(this.chef.address, { from: alice });
             await this.chef.add('1', this.lp.address, true);
             await this.lp.approve(this.chef.address, '1000', { from: bob });
+            console.log("balanceOf 0", (await this.long.balanceOf(bob)).valueOf());
             await this.chef.deposit(0, '100', { from: bob });
+            console.log("0", await time.latestBlock()); //block number 36
+            console.log("balanceOf 1", (await this.long.balanceOf(bob)).valueOf());
             await this.chef.deposit(0, '0', { from: bob }); //1000
-            await this.chef.deposit(0, '0', { from: bob }); //2000
-            await this.chef.deposit(0, '0', { from: bob });
-            await this.chef.deposit(0, '0', { from: bob });
-            await this.chef.deposit(0, '0', { from: bob });
-            await this.chef.deposit(0, '0', { from: bob });
-            await this.chef.deposit(0, '0', { from: bob });
-            await this.chef.deposit(0, '0', { from: bob });
-            await this.chef.deposit(0, '0', { from: bob });
-            await this.chef.deposit(0, '0', { from: bob });
+            console.log("balanceOf 2", (await this.long.balanceOf(bob)).valueOf());
+            console.log("1", await time.latestBlock()); //block number 37
+            await time.advanceBlockTo(56);
+            await this.chef.deposit(0, '0', { from: bob }); //block number 57
+            console.log("balanceOf 3", (await this.long.balanceOf(bob)).valueOf());
+            assert.equal((await this.long.balanceOf(bob)).valueOf(), '21000');
 
-            assert.equal((await this.long.balanceOf(bob)).valueOf(), '10000');
-            assert.equal((await this.long.balanceOf(dev)).valueOf(), '1000');
-            console.log( (await this.long.totalSupply()).valueOf() );
-            assert.equal((await this.long.totalSupply()).valueOf(), '11000');
+            // 900 tokens minted per block
+            await time.advanceBlockTo(77);
+            await this.chef.deposit(0, '0', { from: bob }); // block number 78
+            console.log("balanceOf 4", (await this.long.balanceOf(bob)).valueOf());
+            assert.equal((await this.long.balanceOf(bob)).valueOf(), '39900');
+
+            // 800 tokens minted per block
+            await time.advanceBlockTo(98);
+            await this.chef.deposit(0, '0', { from: bob }); // block number 99
+            console.log("balanceOf 5", (await this.long.balanceOf(bob)).valueOf());
+            assert.equal((await this.long.balanceOf(bob)).valueOf(), '56700');
+
+            // 700 tokens minted per block
+            await time.advanceBlockTo(119);
+            await this.chef.deposit(0, '0', { from: bob }); // block number 120
+            console.log("balanceOf 5", (await this.long.balanceOf(bob)).valueOf());
+            assert.equal((await this.long.balanceOf(bob)).valueOf(), '71400');
+
+            // 600 tokens minted per block
+            await time.advanceBlockTo(140);
+            await this.chef.deposit(0, '0', { from: bob }); // block number 141
+            console.log("balanceOf 5", (await this.long.balanceOf(bob)).valueOf());
+            assert.equal((await this.long.balanceOf(bob)).valueOf(), '84000');
+
+            // 500 tokens minted per block
+            await time.advanceBlockTo(161);
+            await this.chef.deposit(0, '0', { from: bob }); // block number 162
+            console.log("balanceOf 6", (await this.long.balanceOf(bob)).valueOf());
+            assert.equal((await this.long.balanceOf(bob)).valueOf(), '94500');
+
+            // 100 tokens minted per block
+            await time.advanceBlockTo(182);
+            await this.chef.deposit(0, '0', { from: bob }); // block number 183
+            console.log("balanceOf 7", (await this.long.balanceOf(bob)).valueOf());
+            assert.equal((await this.long.balanceOf(bob)).valueOf(), '96600');
         });
 
         it('should give out LONGs only from >= bonusEndBlock', async () => {
@@ -95,18 +127,5 @@ contract('LongChef', ([alice, bob, carol, dev, minter]) => {
             }
             assert.equal((await this.long.balanceOf(bob)).valueOf(), '1000');
         });
-
-        // it('should give out LONGs only block number > 21000', async () => {
-        //     // For this test to work, you need to modify the blocksPerStage parameter in LongChef
-        //     this.chef = await LongChef.new(this.long.address, dev, '100', '22000', '148000', { from: alice });
-        //     await this.long.transferOwnership(this.chef.address, { from: alice });
-        //     await this.chef.add('1', this.lp.address, true);
-        //     await this.lp.approve(this.chef.address, '1000', { from: bob });
-        //     await this.chef.deposit(0, '100', { from: bob });
-        //     for(i=0;i<10;i++) {
-        //         await this.chef.deposit(0, '0', { from: bob });
-        //     }
-        //     assert.equal((await this.long.balanceOf(bob)).valueOf(), '9000');
-        // });
     });
 });
